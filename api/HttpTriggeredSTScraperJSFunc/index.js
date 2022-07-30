ae9d99cce96d51a98c35a0c0ee3b2a5eed267573
Will Goodwin <wsgoodwi@ncsu.edu>
2022-07-30 14:40:24 -0400

    Trying to add my ST code

--- api/HttpTriggeredSTScraperJSFunc/index.js
+++ api/HttpTriggeredSTScraperJSFunc/index.js
@@ -17,3 +17,36 @@ module.exports = async function (context, req) {
         text: "Hello from the API"
     });
 };
+
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+module.exports = async function getIFCCenter(context, req) {
+    try {
+        const response = await axios.get(
+            "https://www.ifccenter.com/"
+        )
+        console.log(response)
+
+        const $ = cheerio.load(response.data)
+        const shows = $(".showtimes") //only 1 of these classes
+        let todayDivs = $(shows[0]).find("div") //123 divs in here
+        let todayDets = $(todayDivs[0]).find(".details") //"details" for all the Movies for Today
+        let allMovieString = ""
+
+        for (let i = 0; i < todayDets.length; i++) {
+            let movieTitleAndTimes = $(todayDets[i]).find("a")
+            let movieTitle = $(movieTitleAndTimes[0]).text()
+            let movieTimes = $(todayDets[i]).find(".times").find("a").text()
+            console.log(movieTitle)
+            console.log(movieTimes)
+            allMovieString += movieTitle + " " + movieTimes + " "
+        }
+
+        context.res.json({
+            text: allMovieString
+        });
+    } catch (error) {
+        console.error(error)
+    }
+}
