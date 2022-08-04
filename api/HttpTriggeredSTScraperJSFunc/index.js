349704b085c260d076013dec45575ce52510e7d4
Will Goodwin <wsgoodwi@ncsu.edu>
2022-08-04 11:44:33 -0400

    Edited ifc scraper code to build title and time array rather than adding directly to the movie string directly. This makes it uniform with the other theaters and it will hopefully help when I eventually figure out how to produce a table for the titles and times instead of one long string.

--- api/HttpTriggeredSTScraperJSFunc/index.js
+++ api/HttpTriggeredSTScraperJSFunc/index.js
@@ -1,28 +1,3 @@
-/** 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        *//* status: 200, *//* Defaults to 200 *//*
-        body: responseMessage
-    };
-}
-*/
-
-/** 
-module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
-*/
-
-
 const axios = require("axios")
 const cheerio = require("cheerio")
 
@@ -32,27 +7,28 @@ module.exports = async function (context, req) {
         const response = await axios.get(
             "https://www.ifccenter.com/"
         )
-        //context.log(response)
 
         const $ = cheerio.load(response.data)
         const shows = $(".showtimes") //only 1 of these classes
         let todayDivs = $(shows[0]).find("div") //123 divs in here
         let todayDets = $(todayDivs[0]).find(".details") //"details" for all the Movies for Today
-        //let allMovieString = ""
+        let movieTitlesArr = []
+        let movieTimesArr = []
 
         allMovieString += "IFC Center:\n--------------\n"
         for (let i = 0; i < todayDets.length; i++) {
             let movieTitleAndTimes = $(todayDets[i]).find("a")
-            let movieTitle = $(movieTitleAndTimes[0]).text()
-            let movieTimes = $(todayDets[i]).find(".times").find("a").text()
-            //console.log(movieTitle)
-            //console.log(movieTimes)
-            allMovieString += movieTitle.toLocaleUpperCase() + ":\n" + movieTimes + "\n"
+            //let movieTitle = $(movieTitleAndTimes[0]).text()
+            //let movieTimes = $(todayDets[i]).find(".times").find("a").text()
+            movieTitlesArr[i] = $(movieTitleAndTimes[0]).text()
+            movieTimesArr[i] = $(todayDets[i]).find(".times").find("a").text()
+            //allMovieString += movieTitle.toLocaleUpperCase() + ":\n" + movieTimes + "\n"
         }
 
-        //context.res.json({
-          //  text: allMovieString
-        //});
+        for (let i = 0; i < movieTitlesArr.length; i++) {
+            allMovieString += movieTitlesArr[i].toLocaleUpperCase() + ":\n" + movieTimesArr[i] + "\n"
+        }
+
     } catch (error) {
         console.error(error)
     }
@@ -61,7 +37,6 @@ module.exports = async function (context, req) {
         const response = await axios.get(
             "https://metrograph.com/calendar/"
         )
-        //console.log(response)
 
         const $ = cheerio.load(response.data)
         const calListDays = $(".calendar-list-day")
@@ -86,15 +61,12 @@ module.exports = async function (context, req) {
 
         allMovieString += "\nMETROGRAPH:\n--------------\n"
         for(let i = 0; i < movieTitlesArr.length; i++) {
-            //console.log(movieTitlesArr[i])
-            //console.log(movieTimesArr[i])
             if (movieTitlesArr[i] && (typeof movieTitlesArr[i] !== 'undefined')) {
                 allMovieString += movieTitlesArr[i].toLocaleUpperCase() + ":\n"
             }
             if (movieTimesArr[i] && (typeof movieTimesArr[i] !== 'undefined')) {
                 allMovieString += movieTimesArr[i].substr(9).toLocaleUpperCase() + "\n"
             }
-            //allMovieString += movieTitlesArr[i] + ": " + movieTimesArr[i] + "\n"
         }
     } catch (error) {
         console.error(error)
@@ -137,31 +109,6 @@ module.exports = async function (context, req) {
             allMovieString += movieTitlesArr[i] + ":\n"
             allMovieString += movieTimesArr[i].substr(9) + "\n"
         }
-
-        /** 
-        const namesArr = $(".name")
-        allMovieString += "\nAngelika Film Center-Angelika New York:\n"
-        for (let i = 0; i < namesArr.length; i++) {
-            //allMovieString += namesArr[i].text()
-        }
-        const nowPlayingArr = $(".film.status-now_playing")
-        const movieTitles = $(".name").find("a")
-        const showtimes = $(".showtimes-wrapper")
-        
-        allMovieString += "\nAngelika Film Center-Angelika New York:\n"
-        
-        for (let i = 0; i < movieTitles.length; i++) {
-            allMovieString += movieTitles[i].text();
-        }
-        for (let i=0; i < nowPlayingArr.length; i++) {
-            allMovieString += nowPlayingArr[i].find(".name").find("a").text().trim()
-            let showtimes = nowPlayingArr[i].find(".showtimes-wrapper").find("a")
-            for (let j = 0; j < showtimes.length; j++) {
-                allMovieString += showtimes[i].text().trim() + " " 
-            }
-            allMovieString += "\n"
-        }
-        */
 
         context.res.json({
             text: allMovieString
