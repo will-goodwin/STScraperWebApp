ed9944119898a558090c16ce1ebf982d6fd56fd5
Will Goodwin <wsgoodwi@ncsu.edu>
2022-08-04 18:13:12 -0400

    Getting rid of the tests for background image since I have removed that. Once I finalize the background design, I'll write new tests for that.

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -5,15 +5,3 @@ test('basic test', async ({ page }) => {
   await page.waitForSelector('h1');
   await expect(page.locator('h1')).toContainText('NYC Showtimes Today!');
 })
-
-test('background img visible test', async ({ page }) => {
-  await page.goto('/');
-  await page.waitForSelector('img');
-  await expect(page.locator('img')).toBeVisible();
-})
-
-test('background img src link test', async ({ page }) => {
-  await page.goto('/');
-  await page.waitForSelector('img');
-  await expect(page.locator('img')).toHaveAttribute('src', 'https://images.pexels.com/photos/2906882/pexels-photo-2906882.jpeg?cs=srgb&dl=pexels-anas-hinde-2906882.jpg&fm=jpg')
-})
