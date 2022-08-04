b99e3dfdf53933b4e873bc3e3691d18a327a6c74
Will Goodwin <wsgoodwi@ncsu.edu>
2022-08-04 10:59:42 -0400

    Added some playwright tests to ensure that the frontend HTML contains the right elements and text and testing to make sure the backend delivers visible content for the p tag, i.e. the showtimes text.

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -5,3 +5,27 @@ test('basic test', async ({ page }) => {
   await page.waitForSelector('h1');
   await expect(page.locator('h1')).toContainText('NYC Showtimes Today!');
 })
+
+test('page title test', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForSelector('title');
+  await expect(page.locator('title')).toContainText('NYC Showtimes');
+})
+
+test('background img visible test', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForSelector('img');
+  await expect(page.locator('img')).toBeVisible();
+})
+
+test('background img src link test', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForSelector('img');
+  await expect(page.locator('img')).toHaveAttribute('src', 'https://images.pexels.com/photos/2906882/pexels-photo-2906882.jpeg?cs=srgb&dl=pexels-anas-hinde-2906882.jpg&fm=jpg')
+})
+
+test('showtime text test', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForSelector('img');
+  await expect(page.locator('p')).toBeVisible()
+})
