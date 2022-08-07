67c4563a05e5021fbedadcf3e1fa7d18f029c5bc
Will Goodwin <wsgoodwi@ncsu.edu>
2022-08-07 13:09:23 -0400

    Eliminated playwright test content for now since they are not relevant to debugging the functionality of my site and they do not conform with the current formatting of my site.

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -1,13 +1,1 @@
 import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('/');
-  await page.waitForSelector('h1');
-  await expect(page.locator('h1')).toContainText('NYC');
-})
-
-test('ifc test', async ({ page }) => {
-  await page.goto('/');
-  await page.waitForSelector('.name');
-  await expect(page.locator('.name')).toContainText('IFC');
-})
